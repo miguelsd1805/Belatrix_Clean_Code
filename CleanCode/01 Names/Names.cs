e847a1198a2b893e88ffccbc6b992d88e3c85940
Miguel Alonso Salas Diaz <miguelsd_180594@outlook.com>
2018-07-16 17:34:09 -0500

    Refactoring of Name.cs file.

--- CleanCode/01 Names/Names.cs
+++ CleanCode/01 Names/Names.cs
@@ -2,16 +2,16 @@
 
 namespace CleanCode.Names
 {
-    public class Names
+    public class GraphicsEditor
     {
-        public Bitmap Method1(string n)
+        public Bitmap BitmapDrawer(string filename)
         {
-            var b = new Bitmap(n);
-            var g = Graphics.FromImage(b);
-            g.DrawString("a", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 0));
-            g.DrawString("b", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 20));
-            g.DrawString("c", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 30));
-            return b;
+            var bitmap = new Bitmap(filename);
+            var graphics = Graphics.FromImage(bitmap);
+            graphics.DrawString("a", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 0));
+            graphics.DrawString("b", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 20));
+            graphics.DrawString("c", SystemFonts.DefaultFont, SystemBrushes.Desktop, new PointF(0, 30));
+            return bitmap;
         }
     }
 }
