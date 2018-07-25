6307060df1becbd9088c257f988c8081f5210427
Miguel Salas <miguelsd180594@gmail.com>
2018-07-24 19:32:07 -0500

    SOLID 01_SingleResponsability refactor.

--- SOLID/01 SingleResponsability/Sample.cs
+++ SOLID/01 SingleResponsability/Sample.cs
@@ -4,14 +4,29 @@ namespace SOLID._01_SingleResponsability
 {
     public class Sample
     {
-        public void Sum(int a, int b)
+        public void SumAndDisplayResult(int a, int b)
         {
-            var c = a + b;
-            var m = "";
-            if (c > 0 && c <= 10) m = "Value between 0 and 10";
-            else if (c > 10 && c <= 20) m = "Value between 11 and 20";
-            else if (c > 20 && c <= 30) m = "Value between 11 and 20";
-            Console.WriteLine(string.Format("The sum is: {0} and is in range {1}", c, m));
+            int sum = Sum(a,b);
+            string range = GetNumberRange(sum);
+            DisplaySumAndRange(sum, range);
+        }
+
+        private int Sum(int a, int b)
+        {
+            return a + b;
+        }
+
+        private void DisplaySumAndRange(int sum, string range)
+        {
+            Console.WriteLine(string.Format("The sum is: {0} and is in range {1}", sum, range));
+        }
+
+        private string GetNumberRange(int number)
+        {
+            if (number > 0 && number <= 10) return "Value between 0 and 10";
+            if (number > 10 && number <= 20) return "Value between 11 and 20";
+            if (number > 20 && number <= 30) return "Value between 11 and 20";
+            else return "Number not supported";
         }
     }
 }
