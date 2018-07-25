26ae9af63873389de872dc0724b5fe7f2d4278f0
Miguel Alonso Salas Diaz <miguelsd_180594@outlook.com>
2018-07-25 14:54:17 -0500

    SOLID 04_InterfaceSegregation refactor.

--- SOLID/04 Interface Segregation/IPrintTasks.cs
+++ SOLID/04 Interface Segregation/IPrintTasks.cs
@@ -2,17 +2,31 @@
 
 namespace SOLID._04_Interface_Segregation
 {
-    public interface IPrintTasks
+    public interface ICommonPrinterTasks //<- Accomplishes more reusability.
     {
         bool PrintContent(string content);
         bool ScanContent(string content);
-        bool FaxContent(string content);
         bool PhotoCopyContent(string content);
+    }
+    public interface ICannonMG2470Tasks : ICommonPrinterTasks //<- Not sure if neccesary.
+    {
+    }
+    public interface IHPLaserJetTasks : ICommonPrinterTasks
+    {
+        bool FaxContent(string content);
         bool PrintDuplexContent(string content);
     }
 
-    public class HPLaserJet : IPrintTasks
+    public class HPLaserJet : IHPLaserJetTasks
     {
+        public bool PrintContent(string content)
+        {
+            Console.WriteLine("Print Done"); return true;
+        }
+        public bool ScanContent(string content)
+        {
+            Console.WriteLine("Scan Done"); return true;
+        }
         public bool FaxContent(string content)
         {
             Console.WriteLine("Fax Done"); return true;
@@ -21,26 +35,14 @@ namespace SOLID._04_Interface_Segregation
         {
             Console.WriteLine("PhotoCopy Done"); return true;
         }
-        public bool PrintContent(string content)
-        {
-            Console.WriteLine("Print Done"); return true;
-        }
         public bool PrintDuplexContent(string content)
         {
             Console.WriteLine("Print Duplex Done"); return true;
         }
-        public bool ScanContent(string content)
-        {
-            Console.WriteLine("Scan Done"); return true;
-        }
     }
 
-    public class CannonMG2470 : IPrintTasks
+    public class CannonMG2470 : ICannonMG2470Tasks
     {
-        public bool PhotoCopyContent(string content)
-        {
-            Console.WriteLine("PhotoCopy Done"); return true;
-        }
         public bool PrintContent(string content)
         {
             Console.WriteLine("Print Done"); return true;
@@ -49,13 +51,9 @@ namespace SOLID._04_Interface_Segregation
         {
             Console.WriteLine("Scan Done"); return true;
         }
-        public bool PrintDuplexContent(string content)
+        public bool PhotoCopyContent(string content)
         {
-            return false;
-        }
-        public bool FaxContent(string content)
-        {
-            return false;
+            Console.WriteLine("PhotoCopy Done"); return true;
         }
     }
 }
